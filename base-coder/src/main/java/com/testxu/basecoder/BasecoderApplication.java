44d629b9388bf527f1aa32ed6acc54d22d7ebeb0
swotXu <mr_xudada@163.com>
2019-09-18 15:29:33 +0800

    随手demo

---
+++ base-coder/src/main/java/com/testxu/basecoder/BasecoderApplication.java
@@ -0,0 +1,13 @@
+package com.testxu.basecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasecoderApplication.class, args);
+	}
+
+}
---
+++ base-coder/src/test/java/com/testxu/basecoder/BasecoderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testxu.basecoder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasecoderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
