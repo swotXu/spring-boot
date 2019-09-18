a39887cc7787b8ef2cb91e6a6273ec721f142188
swotXu <mr_xudada@163.com>
2019-09-18 23:23:41 +0800

    自定义start-spring-boot

---
+++ bbt/src/main/java/bbt/BbtGirl.java
@@ -0,0 +1,31 @@
+package bbt;
+
+public class BbtGirl {
+    private String name;
+    private String age;
+    private Integer length;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+}
